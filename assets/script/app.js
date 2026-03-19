ad16dd6b8d5fe794a7683a2ccc63378dbe2f6d33
Fejiro001 <aberefejiro@gmail.com>
2026-03-18 21:14:03 -0500

    Added a popup message after successful form submission

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,8 +1,24 @@
 "use strict";
 
 const menu = document.querySelector(".burger-menu");
+const closePopupBtn = document.querySelector(".close-btn");
 const navigation = document.getElementById("navigation");
+const popupBox = document.getElementById("success-message");
+const contactForm = document.getElementById("contact-form");
 
 menu.addEventListener("click", () => {
   navigation.classList.toggle("show-menu");
+});
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (contactForm.checkValidity()) {
+    popupBox.classList.add("success-message");
+  } else {
+    contactForm.reportValidity();
+  }
+});
+
+closePopupBtn.addEventListener("click", () => {
+  popupBox.classList.remove("success-message");
 });
